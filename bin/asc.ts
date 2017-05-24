07511986e0ff91ec90e00773349e075b5a236f92
dcodeIO <dcode@dcode.io>
2017-05-24 09:31:41 +0200

    Initial command line interface, asc

---
+++ bin/asc.ts
@@ -0,0 +1,49 @@
+/// <reference path="./require-json.d.ts" />
+
+import { Compiler } from "../src/compiler";
+import * as fs from "fs";
+import * as minimist from "minimist";
+import * as chalk from "chalk";
+import * as pkg from "../package.json";
+
+const argv = minimist(process.argv.slice(2), {
+  alias: {
+    "out": [ "o", "outFile" ],
+    "optimize": [ "O" ]
+  },
+  string: [ "out" ],
+  boolean: [ "text", "optimize" ]
+});
+
+const files = argv._;
+
+if (files.length !== 1) {
+  process.stderr.write([
+    "Version " + pkg["version"],
+    "Syntax: asc [options] [file ...]",
+    "",
+    "Options:",
+    " -o, --out         Specifies the output file name.",
+    " -O, --optimize    Runs optimizing binaryen IR passes.",
+    " --text            Emits text format instead of a binary.",
+    ""
+  ].join("\n"));
+  process.exit(1);
+}
+
+const wasmModule = Compiler.compile(files[0]);
+if (!wasmModule)
+  process.exit(1);
+
+if (argv.optimize)
+  wasmModule.optimize();
+
+if (argv.out && /\.wast$/.test(argv.out))
+  argv.text = true;
+
+let output: any = argv.out ? fs.createWriteStream(argv.out) : process.stdout;
+
+if (argv.text)
+  output.write(wasmModule.emitText(), "utf8");
+else
+  output.write(Buffer.from(wasmModule.emitBinary()));
